fea1758896f3f7f21924cd60944def53f6194495
Sword <62721069+SwordSWD@users.noreply.github.com>
2023-04-28 07:07:46 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,18 @@
 # authors: Sword
 # url: https://github.com/SwordSWD/forlink
 
-require 'resque/tasks'
-task 'resque:setup' => :environment
+gem 'rbnacl', '3.4.0'
+gem 'event_emitter', '0.2.6'
+gem 'websocket', '1.2.8'
+gem 'websocket-client-simple', '0.3.0'
+gem 'opus-ruby', '1.0.1', { require: false }
+gem 'netrc', '0.11.0'
+gem 'mime-types-data', '3.2019.1009'
+gem 'mime-types', '3.3.1'
+gem 'domain_name', '0.5.20180417'
+gem 'http-cookie','1.0.3'
+gem 'http-accept', '1.7.0', { require: false }
+gem 'rest-client', '2.1.0.rc1'
 
 # Add custom route to the Discourse app
 Discourse::Application.routes.append do
@@ -44,4 +54,4 @@ DiscourseEvent.on(:inject_custom_html) do |doc|
   else
     doc.css('header .d-header-buttons').append('<a class="btn btn-primary" href="/requesttoken">Link Account</a>')
   end
-end 
+end
