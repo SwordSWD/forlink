21644045e6cdc60655d5888188504ad9a14db656
Sword <62721069+SwordSWD@users.noreply.github.com>
2023-04-28 06:21:39 -0400

    Initial commit.

---
+++ plugin.rb
@@ -0,0 +1,40 @@
+# plugin.rb
+
+# Add custom route to the Discourse app
+Discourse::Application.routes.append do
+  post 'requesttoken' => 'requesttoken#index'
+end
+
+# Create a new controller to handle the request
+class RequesttokenController < ApplicationController
+  requires_plugin 'my_plugin'
+
+  def index
+    # Make a POST request to the api/client endpoint
+    response = Net::HTTP.post(
+      URI("https://panel.projectmoon.pw/api/client/#{current_user.id}/login"),
+      { password: 'somepassword' }.to_json,
+      'Content-Type' => 'application/json'
+    )
+
+    if response.is_a?(Net::HTTPSuccess)
+      # Handle success response
+      current_user.custom_fields['linked_account'] = "https://panel.projectmoon.pw/Client/Profile/#{response.body}"
+      current_user.save_custom_fields
+
+      render json: { success: true }
+    else
+      # Handle error response
+      render json: { success: false, error: response.message }
+    end
+  end
+end
+
+# Add a custom button to the Discourse interface
+DiscourseEvent.on(:inject_custom_html) do |doc|
+  if current_user && current_user.custom_fields['linked_account'].present?
+    doc.css('header .d-header-buttons').append("<a class='btn btn-primary' href='#{current_user.custom_fields['linked_account']}'>Linked Account</a>")
+  else
+    doc.css('header .d-header-buttons').append('<a class="btn btn-primary" href="/requesttoken">Link Account</a>')
+  end
+end
